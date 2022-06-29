919aaf73783c11a7ee50973c209a809f9a86ace2
nardinegergis <nardinegergis@gmail.com>
2022-06-28 20:27:50 -0400

    complete functionalty to the table

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
-console.log('hello world')
+const grades = [
+    {
+        firstName: "Nardine",
+        lastName: "George",
+        grade: 90
+    },
+    {
+        firstName: "Jude",
+        lastName: "Clark",
+        grade: 99
+    }
+]
+const form = document.getElementById("inputForm")
+const tableBody = document.getElementById("table-body")
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    submitForm()
+})
+
+function submitForm() {
+    const newGrade = {
+        firstName: document.getElementById('fname').value,
+        lastName: document.getElementById('lname').value,
+        grade: document.getElementById('score').value
+    }
+    console.log(newGrade)
+    grades.push(newGrade)
+    renderTable();
+}
+
+function renderTable() {
+    tableBody.innerHTML="";
+    for (let i = 0; i < grades.length; i++) {
+        //create a tr
+        let tr = document.createElement('tr')
+        //set innerHTML of tr to be 3 tds
+        tr.innerHTML = `
+        <td>${grades[i].firstName}</td>
+         <td>${grades[i].lastName}</td>
+         <td>${grades[i].grade}</td>`
+        //append tr to tableBody
+        tableBody.append(tr)
+    }
+
+}
+renderTable();
